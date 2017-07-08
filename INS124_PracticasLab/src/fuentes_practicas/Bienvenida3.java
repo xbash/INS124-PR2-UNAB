6eeaf254f546d54559dcf3156c34e31c49afadee
Jorge Sepulveda S <jorgeantonio.sepulveda@gmail.com>
2017-07-08 13:16:33 -0400

    Actualizando repositorio

--- INS124_PracticasLab/src/fuentes_practicas/Bienvenida3.java
+++
@@ -1,7 +0,0 @@
-//w
-public class Bienvenida3{
-	//el metodo main empieza la ejecucion de la aplicacion java
-	public static void main(String[] args){
-		System.out.println("Bienvenido\na\nla programacion");
-	}
-}
